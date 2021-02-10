9494df28a8a27fe16efbd370693e8e28d5351c2b
Stu Thompson <stuart@stuartthompson.net>
2021-02-09 20:13:01 -0800

    Restructuring code.

--- src/websocket/client.rs
+++ src/websocket/client.rs
@@ -3,17 +3,22 @@ use std::io::{Read, Write};
 use std::sync::mpsc::TryRecvError;
 use http;
 use httpparser::parse_http_request;
+use super::parser;
 
 /**
  * Handles a websocket client.
  */
-pub fn handle_client(mut stream: std::net::TcpStream, address: std::net::SocketAddr, to_server_tx: std::sync::mpsc::Sender<String>, from_server_rx: std::sync::mpsc::Receiver<String>) {
+pub fn handle_client(
+    mut stream: std::net::TcpStream, 
+    address: std::net::SocketAddr, 
+    to_server_tx: std::sync::mpsc::Sender<String>, 
+    from_server_rx: std::sync::mpsc::Receiver<String>) {
     // Spawn a thread to handle the new client
     std::thread::spawn(move || {
         println!("[Server] New client connection from {0}", address);
 
         let mut client_connected: bool = true;
-        let mut data = [0 as u8; 4096]; // using 512 byte buffer
+        let mut data = [0 as u8; 4096];
 
         let mut is_ws = false;
         while client_connected {
@@ -29,7 +34,7 @@ pub fn handle_client(mut stream: std::net::TcpStream, address: std::net::SocketA
                         // for i in 0..size {
                         //     println!("Byte {0: >2} is {1: >3}: {1:0>8b}", i, data[i]);
                         // }
-                        let content = parse_websocket_frame(data, size);
+                        let content = parser::parse_websocket_frame(data, size);
                         println!("Received: {0}", content);
                         // TODO: Parse messages from client (should be JSON)
                         // This should be command-parsing.
@@ -105,33 +110,4 @@ pub fn handle_client(mut stream: std::net::TcpStream, address: std::net::SocketA
 
         println!("[Client @ {0}] Client disconnected.", address);
     });
-}
-
-/*
- * Parses a websocket frame.
- */
-pub fn parse_websocket_frame(content: [u8; 4096], size: usize) -> String {
-    let _fin_bit: bool = (content[0] & 0b10000000) != 0; // Bit 0 has fin bit
-    let _rsv1: bool = (content[0] & 0b01000000) != 0; // Bit 1 contains reserved flag 1
-    let _rsv2: bool = (content[0] & 0b00100000) != 0; // Bit 2 contains reserved flag 2
-    let _rsv3: bool = (content[0] & 0b00010000) != 0; // Bit 3 contains reserved flag 3
-    let _opcode = (content[0] & 0b00001111) as u8; // Bits 4 - 7 contain opcode (1-3 are reserved)
-    let _mask_bit: bool = (content[1] & 0b10000000) != 0; // Bit 8 contains mask flag
-    let _payload_len = (content[1] & 0b01111111) as u8; // Bits 9 - 15 contain payload length
-
-    // TODO: Handle case where payload length > 126
-
-    // Next 32-bits define the mask (in short payload case)
-    let mask:[u8; 4] = [content[2], content[3], content[4], content[5]];
-
-    // Decode payload content (XOR payload bits with mask bits)
-    let mut decoded: Vec<u8> = Vec::new();
-    for i in 0..size-6 {
-        decoded.push(content[6+i] ^ mask[i % 4]); // 32 mask bits are used repeatedly
-    }
-
-    // Convert decoded payload into string
-    let result = std::str::from_utf8(&decoded).expect("Error decoding websocket payload.");
-    
-    return String::from(result);
 }
